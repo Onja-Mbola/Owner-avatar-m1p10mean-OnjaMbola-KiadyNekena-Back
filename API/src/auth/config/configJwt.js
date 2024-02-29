5385150053b3e695c1131a8d354be7e9f63a8ae4
Kiady Nekena <72613759+KiadyBreezy@users.noreply.github.com>
2024-03-01 00:44:32 +0300

    Config LIEN CRASH

--- API/src/auth/config/configJwt.js
+++ API/src/auth/config/configJwt.js
@@ -1,6 +1,6 @@
 module.exports = {
   secret: "manjamanja",
   secretAdmin: "manjamanja",
-  lien: 'https://m1p10mean-onjambola-kiadynekena-back-2.onrender.com/',
+  lien: 'https://m1p10mean-onjambola-kiadynekena-back-2.onrender.com',
   loginlien: 'https://m1p10mean-onja-mbola-kiady-nekena-back-e6j9.vercel.app/login'
 };
